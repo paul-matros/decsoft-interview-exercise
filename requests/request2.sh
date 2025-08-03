78671e3441ce4d70efa33300c03cd93bcc5d5e77
Paul <paul.matros@gmail.com>
2025-08-03 23:35:03 +0200

    fix: shorten subject in request2 payload to pass validation

--- requests/request2.sh
+++ requests/request2.sh
@@ -2,6 +2,6 @@ curl -o - -X POST http://host.docker.internal:8080/api/authors/2/contact \
 -u "user:example" \
 -H "Content-Type: application/json" \
 -d '{
-  "subject": "veryLongSubjectBecauseIForgotToTypeTabAndImStillWritingItHereButItIsTooLong11111111111111111111116666666666666666666666666666",
+  "subject": "notSoLongSubject",
   "message": "message"
 }' -s -o /dev/null -w "%{http_code}\n"

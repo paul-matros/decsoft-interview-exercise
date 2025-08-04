030f1b53b57a2b7dfd2ef36809f2073a2378bef0
Paul <paul.matros@gmail.com>
2025-08-04 23:45:45 +0200

    cleanup: remove validation from ContactFormResponseDTO

--- src/main/java/com/pl/example/author/dto/ContactFormResponseDTO.java
+++ src/main/java/com/pl/example/author/dto/ContactFormResponseDTO.java
@@ -1,12 +1,8 @@
 package com.pl.example.author.dto;
 
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-
 public record ContactFormResponseDTO(
-        @NotNull @NotEmpty String subject,
-        @Size(min = 3, max = 70) @NotNull @NotEmpty String message,
-        @NotNull String author
+        String subject,
+        String message,
+        String author
 ) {
 }

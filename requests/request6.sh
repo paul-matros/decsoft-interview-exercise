0a78ec0e25459e5a440959fdb86c0c253f66b3ca
Paul <paul.matros@gmail.com>
2025-08-05 22:01:47 +0200

    fix: adjust request6.sh to still cause same exception but uses new api allowing for multiple items in orders

--- requests/request6.sh
+++ requests/request6.sh
@@ -2,8 +2,12 @@ curl -o - -X POST 'http://host.docker.internal:8080/api/orders' \
 -u "user:example" \
 -H "Content-Type: application/json" \
 -d '{
-  "isbn": "9780618640157",
-  "quantity": 0,
-  "customerId": 4
+  "customerId": 4,
+    "orderItems": [
+      {
+        "isbn": "9780618640157",
+        "quantity": 0
+      }
+    ]
 }' \
 -s -o /dev/null -w "%{http_code}\n"

0d6e593a84381b66965e5a2dd58948be503a28d7
Paul <paul.matros@gmail.com>
2025-08-05 18:46:28 +0200

    fix: make book order date filter inclusive of both fromDate and toDate

--- src/main/java/com/pl/example/order/repository/BookOrderRepository.java
+++ src/main/java/com/pl/example/order/repository/BookOrderRepository.java
@@ -1,20 +1,20 @@
 package com.pl.example.order.repository;
 
-import java.time.LocalDate;
-import java.util.List;
-
 import com.pl.example.order.dto.BookOrderReportItem;
 import com.pl.example.order.model.BookOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface BookOrderRepository extends JpaRepository<BookOrder, Long> {
     @Query("SELECT new com.pl.example.order.dto.BookOrderReportItem(bor.isbn, bor.title, SUM(bor.quantity)) " +
             "FROM BookOrderReport bor " +
             "WHERE bor.orderDate >= :fromDate " +
-              "AND bor.orderDate < :toDate " +
+            "AND bor.orderDate <= :toDate " +
             "GROUP BY bor.isbn, bor.title")
     List<BookOrderReportItem> getBookOrdersByDate(LocalDate fromDate, LocalDate toDate);
 }
